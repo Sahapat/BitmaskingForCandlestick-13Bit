6a66ece69a8a0a88e4aef15aed7bafe6e117c911
Sahapat <sahapat.starcaft@gmail.com>
2017-06-23 00:36:58 +0700

    Optimize SetNormal function

--- CandleStick/SetNormalData.cs
+++ CandleStick/SetNormalData.cs
@@ -13,10 +13,7 @@ namespace CandleStick
         private CandleNormalData[] normalData = new CandleNormalData[0];
         private int StartPoint;
 
-        private const byte High = 3;
-        private const byte Mid = 2;
-        private const byte Low = 1;
-        private const byte None = 0;
+        private short SizePerUnit = 1; //It's mean 1 point per unit
 
         public CandleNormalData[] getNormalData(BigInteger[] package,int startPoint)
         {
@@ -40,111 +37,17 @@ namespace CandleStick
             for(int i =0;i< normalData.Length;i++)
             {
                 normalData[i].Low = StartPoint;
-                if (status[i].Direction == (int)TypeTrend.UP)
+                if(status[i].Direction == (int)TypeTrend.UP)
                 {
-                    if (status[i].LowerShadow == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].Open = normalData[i].Low + High;
-                    }
-                    else if (status[i].LowerShadow == (int)TypeCandle.MID)
-                    {
-                        normalData[i].Open = normalData[i].Low + Mid;
-                    }
-                    else if (status[i].LowerShadow == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].Open = normalData[i].Low + Low;
-                    }
-                    else
-                    {
-                        normalData[i].Open = normalData[i].Low + None;
-                    }
-
-                    if (status[i].Body == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].Close = normalData[i].Open + High;
-                    }
-                    else if (status[i].Body == (int)TypeCandle.MID)
-                    {
-                        normalData[i].Close = normalData[i].Open + Mid;
-                    }
-                    else if (status[i].Body == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].Close = normalData[i].Open + Low;
-                    }
-                    else
-                    {
-                        normalData[i].Close = normalData[i].Open + None;
-                    }
-
-                    if (status[i].UpperShadow == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].High = normalData[i].Close + High;
-                    }
-                    else if (status[i].UpperShadow == (int)TypeCandle.MID)
-                    {
-                        normalData[i].High = normalData[i].Close + Mid;
-                    }
-                    else if (status[i].UpperShadow == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].High = normalData[i].Close + Low;
-                    }
-                    else
-                    {
-                        normalData[i].High = normalData[i].Close + None;
-                    }
+                    normalData[i].Open = normalData[i].Low+(status[i].LowerShadow * SizePerUnit);
+                    normalData[i].Close = normalData[i].Open + (status[i].Body * SizePerUnit);
+                    normalData[i].High = normalData[i].Close + (status[i].UpperShadow * SizePerUnit);
                 }
                 else
                 {
-                    if (status[i].LowerShadow == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].Close = normalData[i].Low + High;
-                    }
-                    else if (status[i].LowerShadow == (int)TypeCandle.MID)
-                    {
-                        normalData[i].Close = normalData[i].Low + Mid;
-                    }
-                    else if (status[i].LowerShadow == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].Close = normalData[i].Low + Low;
-                    }
-                    else
-                    {
-                        normalData[i].Close = normalData[i].Low + None;
-                    }
-
-                    if (status[i].Body == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].Open = normalData[i].Close + High;
-                    }
-                    else if (status[i].Body == (int)TypeCandle.MID)
-                    {
-                        normalData[i].Open = normalData[i].Close + Mid;
-                    }
-                    else if (status[i].Body == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].Open = normalData[i].Close + Low;
-                    }
-                    else
-                    {
-                        normalData[i].Open = normalData[i].Close + None;
-                    }
-
-                    if (status[i].UpperShadow == (int)TypeCandle.HIGH)
-                    {
-                        normalData[i].High = normalData[i].Open + High;
-                    }
-                    else if (status[i].UpperShadow == (int)TypeCandle.MID)
-                    {
-                        normalData[i].High = normalData[i].Open + Mid;
-                    }
-                    else if (status[i].UpperShadow == (int)TypeCandle.LOW)
-                    {
-                        normalData[i].High = normalData[i].Open + Low;
-                    }
-                    else
-                    {
-                        normalData[i].High = normalData[i].Open + None;
-                    }
+                    normalData[i].Close = normalData[i].Low + (status[i].LowerShadow * SizePerUnit);
+                    normalData[i].Open = normalData[i].Close + (status[i].Body * SizePerUnit);
+                    normalData[i].High = normalData[i].Close + (status[i].UpperShadow * SizePerUnit);
                 }
             }
             
